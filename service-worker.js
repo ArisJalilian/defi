cbcd2f57234c5f6b7fb138fe33616c35b61d976e
Aris <aris@agencywolfe.com>
2020-10-31 01:17:56 +0330

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/defi/precache-manifest.6de3a28fd4a11cf9be19923afccb97d7.js"
+  "/defi/precache-manifest.ff21bd0d15038740cf26dba4ac5c6d32.js"
 );
 
 self.addEventListener('message', (event) => {
